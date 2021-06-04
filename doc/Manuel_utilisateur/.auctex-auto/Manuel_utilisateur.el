7a722268c0cf5e4a0da0c3938a468e06a168fd7f
Pierre LOTTE <pierrelotte.dev@gmail.com>
2021-06-04 16:45:03 +0200

    Add screenshot interface

---
+++ doc/Manuel_utilisateur/.auctex-auto/Manuel_utilisateur.el
@@ -0,0 +1,34 @@
+(TeX-add-style-hook
+ "Manuel_utilisateur"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("report" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin=2.5cm") ("babel" "french") ("fontenc" "T1") ("titlesec" "explicit") ("inputenc" "utf8x")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "titlepage"
+    "report"
+    "rep11"
+    "geometry"
+    "babel"
+    "fontenc"
+    "titlesec"
+    "times"
+    "hyperref"
+    "fancyhdr"
+    "graphicx"
+    "ucs"
+    "inputenc"
+    "awesomebox"
+    "fontawesome5"))
+ :latex)
+
