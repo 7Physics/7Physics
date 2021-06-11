1f2a47c4d0d1bf6fdcb866256f06adcd9cd22279
Feavy <quentin071100@live.fr>
2021-06-11 19:32:09 +0200

    Small refacto. Use latest commits.

--- src/main/java/fr/setphysics/setphysics/Main.java
+++ src/main/java/fr/setphysics/setphysics/Main.java
@@ -2,21 +2,11 @@ package fr.setphysics.setphysics;
 
 import java.awt.EventQueue;
 
-import fr.setphysics.common.logger.Logger;
 import fr.setphysics.setphysics.gui.GUI;
 
-import javax.swing.*;
-
 public class Main {
-      
 
     public static void main(String [] args){
-    	try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-				| UnsupportedLookAndFeelException e) {
-			e.printStackTrace();
-		}
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 GUI.getInstance().setVisible(true);
